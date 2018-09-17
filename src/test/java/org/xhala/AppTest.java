ce98bb9912741f7661aca66600b3c7236230cbc2
Petr Hála <xhala@fi.muni.cz>
2018-09-17 16:47:10 +0200

    Initial commit

---
+++ src/test/java/org/xhala/AppTest.java
@@ -0,0 +1,20 @@
+package org.xhala;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
